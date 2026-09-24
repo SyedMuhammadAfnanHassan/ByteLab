a85d021d6454f44990bbb5d2da4769364d80fc0b
Syed Muhammad Afnan Hassan <syedmuhammadafnan16@gmail.com>
2026-09-24 19:12:14 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.BYTE_LAB_CONFIG = {
+  endpoint: "https://script.google.com/macros/s/AKfycbwRFQi2VG7EZGLH-F-G7xAJRtfGnqgoOlzDiYaGmnn3bj-PG-xlIOTbyE8VGyrEHFcV/exec",
+  gaMeasurementId: "G-GMJ18W3Z9S"
+};
